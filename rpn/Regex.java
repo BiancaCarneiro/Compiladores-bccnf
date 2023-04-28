74b658e0036dd683ec7713f062828d0a9171a7f6
BiancaCarneiro <bianca.ccnf@gmail.com>
2023-04-27 21:04:55 -0300

    Entrega 3

--- rpn/Regex.java
+++ rpn/Regex.java
@@ -1,50 +1,24 @@
 package rpn;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
 	public static boolean isNum(String token) {
-        for (int i = 0; i < token.length(); i++) {
-            switch (token.charAt(i)) {
-                case '0':
-                    return true;
-                case '1':
-                    return true;
-                case '2':
-                    return true;
-                case '3':
-                    return true;
-                case '4':
-                    return true;
-                case '5':
-                    return true;
-                case '6':
-                    return true;
-                case '7':
-                    return true;
-                case '8':
-                    return true;
-                case '9':
-                    return true;
-            default:
-                    return false;
-            }
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(token);
+
+        if (matcher.matches()){
+            return true;
         }
         return false;
     }
 	
 	public static boolean isOP(String token) {
-        for (int i = 0; i < token.length(); i++) {
-            switch (token.charAt(i)) {
-                case '+':
-                    return true;
-                case '-':
-                    return true;
-                case '*':
-                    return true;
-                case '/':
-                    return true;
-                default:
-                    return false;
-            }
+        Pattern pattern = Pattern.compile("[\\+\\-\\*\\/]");
+        Matcher matcher = pattern.matcher(token);
+        if (matcher.matches()){
+            return true;
         }
         return false;
     }
